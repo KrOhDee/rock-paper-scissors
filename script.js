4197061aaacde04fda05b5827a3bd9a2eb614a0e
Luis Aleman <lalem014@fiu.edu>
2022-08-10 15:46:58 -0400

    Redo all code
    Redid all of the code after doing more research.

--- script.js
+++ script.js
@@ -1,79 +1,85 @@
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return 'rock';
-    }
-    else if (randomNumber === 1) {
-        return 'paper';
-    }
-    else if (randomNumber === 2) {
-        return 'scissors';
+let compScore = 0
+let playerScore = 0
 
+const computerPlay = () => {
+    const arrOfChoices = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * 3);
+    const compChoice = arrOfChoices[randomNum];
+    return compChoice;
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'You tied! You both picked rock.'
     }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {    
+        playerScore++
+        return 'You win! Rock beats scissors!'
         
-}
-
-let computerSelection = getComputerChoice()
-
-
-
-
-
-let playerSelection = 'sciSsors';
-
-
-
-function rockPaperScissors(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return 'Draw'
     }
-
-    else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
-        return 'You lose, paper beats rock'
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {    
+        compScore++
+        return 'You lose! paper beats rock!'
+        
     }
-
-    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
-        return 'You win, paper beats rock'
+   else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You tied! You both picked paper.'
     }
-
-    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
-        return 'You win, scissors beats paper'
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {    
+        playerScore++
+        return 'You win! Paper beats rock!'
+        
     }
-
-    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
-        return 'You lose, scissors beats paper'
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {    
+        compScore++
+        return 'You lose! Scissors beats paper!'
+        
     }
-
-    else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
-        return 'You win, rock beats scissors'
+    else if (playerSelection === 'scissors' && computerSelection === 'scissors') {   
+        return 'You tied! You both picked scissors' 
     }
-
-    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
-        return 'You lose, rock beats scissors'
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {    
+        playerScore++
+        return 'You win! Scissors beats paper!'
+        
     }
-
-    else
-        return 'Error'
-}
-
-
-console.log(rockPaperScissors(playerSelection, computerSelection));
-
-
-function game() {
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {    
+        compScore++
+        return 'You lose! Rock beats scissors!'
+        
+    }
 
 }
 
 
 
 
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt('Rock, paper, or scissors?', 'Choose').toLowerCase();
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+}
+ 
+if (playerScore < compScore) {
+    return 'You lost to the computer.'
+}
+else if (playerScore > compScore) {
+    return 'You beat the computer.'
+}
+else {
+    return 'You tied with the computer.'
+}
+}
 
+console.log(game());
 
 
 
 
 
 
+ 
 
 
 
@@ -121,38 +127,25 @@ function game() {
 
 
 
-//function getComputerChoice () {
-   // let randomNumber = (Math.floor(Math.random()*3));
-   
-   // switch (randomNumber) {
-     //   case 0:
-     //   return 'rock';
 
-     //   case 1: 
-    //    return 'paper';
 
-    //    case 2: 
-    //    return 'scissors';
-   // }
 
 
 
 
 
 
-   // if (randomNumber === 0){
-   //     return 'Rock';
-   //   }
-   //   else if (randomNumber === 1){
-  //      return 'Paper';
-   //   }
-  //    else if (randomNumber === 2){
-  //    return 'Scissors';
-   //   }
-//}
 
 
-//console.log (getComputerChoice())
 
-//use math.random to select an integer from 0 to 1 and let it be multiplied by 3 to give you the numbers 0 to 2 at random
-//assign the random numbers to return rock paper or scissors
+
+
+
+
+
+
+
+
+
+
+
